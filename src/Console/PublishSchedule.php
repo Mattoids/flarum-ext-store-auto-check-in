2617f816eec693985a1c969da869072a660af65e
mattoid <liufei@liufei.info>
2024-07-05 13:34:05 +0800

    修改 自动签到时间为每天 00:10

--- src/Console/PublishSchedule.php
+++ src/Console/PublishSchedule.php
@@ -13,8 +13,8 @@ class PublishSchedule
         $settingTimezone = $settings->get('mattoid-store.storeTimezone', 'Asia/Shanghai');
 
         // 设置时间
-        $event->everyMinute()->withoutOverlapping()->timezone($settingTimezone);
-//        $event->dailyAt('00:10')->withoutOverlapping()->timezone($settingTimezone);
+//        $event->everyMinute()->withoutOverlapping()->timezone($settingTimezone);
+        $event->dailyAt('00:10')->withoutOverlapping()->timezone($settingTimezone);
 
         $paths = resolve(Paths::class);
         $event->appendOutputTo($paths->storage.(DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR.'mattoid-store-auto-check-in.log'));
